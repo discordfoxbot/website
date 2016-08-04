1304466c07afe910ec362049f43df05630c334db
Fuechschen <me@fuechschen.org>
2016-08-05 01:31:30 +0200

    typo fix

--- routes/dmca.js
+++ routes/dmca.js
@@ -1,7 +1,7 @@
 var app = require('express').Router();
 
 app.get('/', (req, res)=> {
-    res.render('dmca', {pagetitle: 'DCMA', header: {button: [{link: '/', text: 'Home'}]}})
+    res.render('dmca', {pagetitle: 'DMCA', header: {button: [{link: '/', text: 'Home'}]}})
 });
 
 module.exports = app;
