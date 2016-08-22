2d5272d72f5047887553389adb04f711e24908f6
Fuechschen <me@fuechschen.org>
2016-08-23 01:20:39 +0200

    i want to talk about gulp with you ,sir

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp'),
     del = require('del'),
     cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    //jsoneditor = require('gulp-json-editor'),
-    //revAll = require('gulp-rev-all'),
-    //jsonmerge = require('gulp-merge-json'),
-    //jsonminify = require('gulp-jsonminify'),
     obfuscate = require('gulp-js-obfuscator'),
     uglify = require('gulp-uglify');
 
@@ -18,11 +14,6 @@ gulp.task('clean', function () {
 });
 
 gulp.task('scripts', function () {
-    /*var rev = new revAll({
-     replacer: function () {
-
-     }
-     });*/
     return gulp.src('public_source/scripts/*.js')
         .pipe(uglify({
             compress: {
@@ -32,35 +23,14 @@ gulp.task('scripts', function () {
         }))
         .pipe(obfuscate())
         .pipe(rename({suffix: '.min'}))
-        //.pipe(rev.revision())
         .pipe(gulp.dest('public/scripts'));
-    /* .pipe(rev.manifestFile())
-     .pipe(jsoneditor(function (json) {
-     var keys = Object.keys(json);
-     keys.forEach(function (key) {
-     json[key] = '/assets/js/' + json[key];
-     });
-     return json;
-     }))
-     .pipe(gulp.dest('assets/paths/js'));*/
 });
 
 gulp.task('stylesheets', function () {
-    //var rev = new revAll();
     return gulp.src('public_source/stylesheets/*.css')
         .pipe(cleanCSS({debug: true}))
         .pipe(rename({suffix: '.min'}))
-        //.pipe(rev.revision())
         .pipe(gulp.dest('public/stylesheets'));
-    /*.pipe(rev.manifestFile())
-     .pipe(jsoneditor(function (json) {
-     var keys = Object.keys(json);
-     keys.forEach(function (key) {
-     json[key] = '/assets/css/' + json[key];
-     });
-     return json;
-     }))
-     .pipe(gulp.dest('assets/paths/css'));*/
 });
 
 gulp.task('copy', ['copy_fonts', 'copy_images']);
