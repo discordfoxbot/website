1c3077f5f1c5f00b174894f4b6086140d9289f82
Johannes Diehl <johannes@diehl.io>
2017-02-03 02:07:18 +0100

    hopefully datadog likes this

--- metrics.js
+++ metrics.js
@@ -1,6 +1,8 @@
 let metrics = require('datadog-metrics'),
     config = require('./config');
 
+process.env.DATADOG_API_KEY = config.datadog;
+
 metrics.init({apiKey: config.datadog, host: 'kitsune.fuechschen.org', prefix: 'kitsune.web.'});
 
 module.exports = metrics;
