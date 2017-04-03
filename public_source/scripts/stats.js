af2ae79437f669d82088bce604be86fa9a75ee24
Fuechschen <me@fuechschen.org>
2017-04-03 16:48:44 +0200

    rebuild with rounding

--- public_source/scripts/stats.js
+++ public_source/scripts/stats.js
@@ -3,9 +3,9 @@ $(() => {
         $('#stats_mpm').html(Math.round(d.value));
     });
     $.getJSON('/cors-proxy/datadog/guilds',d=>{
-        $('#stats_guilds').html(d.value);
+        $('#stats_guilds').html(Math.round(d.value));
     });
     $.getJSON('/cors-proxy/datadog/users',d=>{
-        $('#stats_users').html(d.value);
+        $('#stats_users').html(Math.round(d.value));
     });
 });
