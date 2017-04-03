8e5f84981b11a4368e3c67d10e83a658fc892023
Fuechschen <me@fuechschen.org>
2017-04-03 16:34:53 +0200

    using the cors proxy

--- public_source/scripts/stats.js
+++ public_source/scripts/stats.js
@@ -1,14 +1,11 @@
 $(() => {
-    $.getJSON('https://app.datadoghq.com/series/query_value_embed?q=per_minute(avg%3Akitsune.bot.messages%7B*%7D)&from=1491223436646&to=1491227036646&aggregator=avg&token=266b0e64c28222f29547ce10d3cc7c166ce3b6938e2831096390b5e83d1d5d32', d => {
+    $.getJSON('/cors-proxy/datadog/mpm', d => {
         $('#stats_mpm').html(Math.round(d.value));
     });
-    $.getJSON('https://app.datadoghq.com/series/query_value_embed?q=avg%3Akitsune.bot.guilds%7B*%7D&from=1491223721915&to=1491227321915&aggregator=avg&token=01e21fd6c8c69b1c40fa64ae66a701a7039cf80096d9aeb99a3448ba6beb7044',d=>{
+    $.getJSON('/cors-proxy/datadog/guilds',d=>{
         $('#stats_guilds').html(d.value);
     });
-    $.getJSON('ttps://app.datadoghq.com/series/query_value_embed?q=avg%3Akitsune.bot.users%7B*%7D&from=1491223820359&to=1491227420359&aggregator=avg&token=18016733827d73a796a0dced1f9aa7aa39e48489f05bba616f5425be17958935',d=>{
+    $.getJSON('/cors-proxy/datadog/users',d=>{
         $('#stats_users').html(d.value);
-    });
-    $.getJSON('\/\/todo',d=>{
-        $('#stats_channels').html(d.value);
     });
 });
